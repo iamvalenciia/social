55cd678438d4a3f042995e8a3501fc403297c399
iamvalenciia <val22018@byui.edu>
2023-03-03 03:50:39 -0500

    working to make the home page responsive

--- src/pages/home/posts.tsx
+++ src/pages/home/posts.tsx
@@ -1,80 +1,90 @@
-import {Navbar} from "./navbar";
-import {Infobar} from "./infobar";
-
+import { Navbar } from "./navbar";
 
 export const Posts = (): JSX.Element => {
-
-    return (
-        <div className="lg:col-span-6 border-2 lg:w-full px-4 border-gray-300">
-            <div>
-                <h2 className="my-4"> Home </h2>
-            </div>
-            <div>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?</p>
-                <br/>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?</p>
-                <br/>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?</p>
-                <br/>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Repellendus consectetur quas sunt magnam eos dolores molestias 
-                fugit! Consectetur aperiam, neque minus facere, tempora nobis
-                doloribus dolores quo sed esse rerum?</p>
-            </div>
+  return (
+    <div className="md:col-span-4 lg:col-span-6 lg:w-full relative inline-block">
+      <div className="sticky top-0">
+        <div className="text-center py-4 text-2xl bg-gray-200 bg-opacity-70 backdrop-filter backdrop-blur-sm">
+          <h1 className="">LightLines</h1>
         </div>
-    )
-}
+      </div>
+      <div className="px-4">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus
+          consectetur quas sunt magnam eos dolores molestias fugit! Consectetur
+          aperiam, neque minus facere, tempora nobis doloribus dolores quo sed
+          esse rerum?
+        </p>
+        <br />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus
+          consectetur quas sunt magnam eos dolores molestias fugit! Consectetur
+          aperiam, neque minus facere, tempora nobis doloribus dolores quo sed
+          esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Repellendus consectetur quas sunt magnam eos dolores molestias fugit!
+          Consectetur aperiam, neque minus facere, tempora nobis doloribus
+          dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Repellendus consectetur quas sunt magnam eos dolores
+          molestias fugit! Consectetur aperiam, neque minus facere, tempora
+          nobis doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Repellendus consectetur quas sunt magnam
+          eos dolores molestias fugit! Consectetur aperiam, neque minus facere,
+          tempora nobis doloribus dolores quo sed esse rerum?Lorem ipsum, dolor
+          sit amet consectetur adipisicing elit. Repellendus consectetur quas
+          sunt magnam eos dolores molestias fugit! Consectetur aperiam, neque
+          minus facere, tempora nobis doloribus dolores quo sed esse rerum?Lorem
+          ipsum, dolor sit amet consectetur adipisicing elit. Repellendus
+          consectetur quas sunt magnam eos dolores molestias fugit! Consectetur
+          aperiam, neque minus facere, tempora nobis doloribus dolores quo sed
+          esse rerum?
+        </p>
+        <br />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus
+          consectetur quas sunt magnam eos dolores molestias fugit! Consectetur
+          aperiam, neque minus facere, tempora nobis doloribus dolores quo sed
+          esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Repellendus consectetur quas sunt magnam eos dolores molestias fugit!
+          Consectetur aperiam, neque minus facere, tempora nobis doloribus
+          dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Repellendus consectetur quas sunt magnam eos dolores
+          molestias fugit! Consectetur aperiam, neque minus facere, tempora
+          nobis doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Repellendus consectetur quas sunt magnam
+          eos dolores molestias fugit! Consectetur aperiam, neque minus facere,
+          tempora nobis doloribus dolores quo sed esse rerum?Lorem ipsum, dolor
+          sit amet consectetur adipisicing elit. Repellendus consectetur quas
+          sunt magnam eos dolores molestias fugit! Consectetur aperiam, neque
+          minus facere, tempora nobis doloribus dolores quo sed esse rerum?Lorem
+          ipsum, dolor sit amet consectetur adipisicing elit. Repellendus
+          consectetur quas sunt magnam eos dolores molestias fugit! Consectetur
+          aperiam, neque minus facere, tempora nobis doloribus dolores quo sed
+          esse rerum?
+        </p>
+        <br />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus
+          consectetur quas sunt magnam eos dolores molestias fugit! Consectetur
+          aperiam, neque minus facere, tempora nobis doloribus dolores quo sed
+          esse rerum?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Repellendus consectetur quas sunt magnam eos dolores molestias fugit!
+          Consectetur aperiam, neque minus facere, tempora nobis doloribus
+          dolores quo sed esse rerum?Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Repellendus consectetur quas sunt magnam eos dolores
+          molestias fugit! Consectetur aperiam, neque minus facere, tempora
+          nobis doloribus dolores quo sed esse rerum?Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Repellendus consectetur quas sunt magnam
+          eos dolores molestias fugit! Consectetur aperiam, neque minus facere,
+          tempora nobis doloribus dolores quo sed esse rerum?Lorem ipsum, dolor
+          sit amet consectetur adipisicing elit. Repellendus consectetur quas
+          sunt magnam eos dolores molestias fugit! Consectetur aperiam, neque
+          minus facere, tempora nobis doloribus dolores quo sed esse rerum?Lorem
+          ipsum, dolor sit amet consectetur adipisicing elit. Repellendus
+          consectetur quas sunt magnam eos dolores molestias fugit! Consectetur
+          aperiam, neque minus facere, tempora nobis doloribus dolores quo sed
+          esse rerum?
+        </p>
+      </div>
+    </div>
+  );
+};
