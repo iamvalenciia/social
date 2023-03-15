57323be08dcbe5ff32ff5748e7b6aa777ba4ee61
iamvalenciia <val22018@byui.edu>
2023-03-14 22:04:39 -0500

    something

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -11,12 +11,12 @@ interface FirebaseConfig {
 }
 
 const firebaseConfig: FirebaseConfig = {
-  apiKey: import.meta.env.VITE_REACT_APP_API_KEY as string,
-  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN as string,
-  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID as string,
-  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET as string,
-  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID as string,
-  appId: import.meta.env.VITE_REACT_APP_APP_ID as string,
+  apiKey: process.env.VITE_REACT_APP_API_KEY as string,
+  authDomain: process.env.VITE_REACT_APP_AUTH_DOMAIN as string,
+  projectId: process.env.VITE_REACT_APP_PROJECT_ID as string,
+  storageBucket: process.env.VITE_REACT_APP_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.VITE_REACT_APP_MESSAGING_SENDER_ID as string,
+  appId: process.env.VITE_REACT_APP_APP_ID as string,
 };
 
 export const app = initializeApp(firebaseConfig);
